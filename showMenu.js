f262c67ac35c1c28a581bcd99787785040fe034b
Kae Verens <kae.verens@fieldmotion.com>
2018-10-16 13:50:24 +0000

    add menu items for other pages

--- showMenu.js
+++ showMenu.js
@@ -1,11 +1,35 @@
-fm.fns.showMenu=function() {
+fm.fns.showMenu=()=>{
 	// { menu HTML
 	var html='<ul class="fm-nav">';
 	html+='<li><a href="#" class="fm-link-list">Jobs List</a></li>';
+	html+='<li><a href="#" class="fm-link-calendar">Calendar</a></li>';
+	html+='<li><a href="#" class="fm-link-assets">Assets</a></li>';
+	html+='<li><a href="#" class="fm-link-files">Files</a></li>';
+	html+='<li><a href="#" class="fm-link-invoices">Invoices</a></li>';
 	html+='<li><a href="#" class="fm-link-logout">Log Out</a></li>';
 	html+='</ul>';
 	// }
 	$('#fm-menu').html(html)
+		.on('click', '.fm-link-assets', function() {
+			fm.fns.whenFunctionsExist(['showAssets'], function() {
+				fm.fns.showAssets();
+			});
+		})
+		.on('click', '.fm-link-calendar', function() {
+			fm.fns.whenFunctionsExist(['showJobsCalendar'], function() {
+				fm.fns.showJobsCalendar();
+			});
+		})
+		.on('click', '.fm-link-files', function() {
+			fm.fns.whenFunctionsExist(['showFiles'], function() {
+				fm.fns.showFiles();
+			});
+		})
+		.on('click', '.fm-link-invoices', function() {
+			fm.fns.whenFunctionsExist(['showInvoices'], function() {
+				fm.fns.showInvoices();
+			});
+		})
 		.on('click', '.fm-link-list', function() {
 			fm.fns.whenFunctionsExist(['showJobsList'], function() {
 				fm.fns.showJobsList();
