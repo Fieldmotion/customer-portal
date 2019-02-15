215bdde03c93fdb6697f6eb275368ffb208aa34b
Kae Verens <kae.verens@fieldmotion.com>
2019-02-15 11:11:08 +0000

    fixes issue crm#98

--- popupJobNew.js
+++ popupJobNew.js
@@ -1,53 +1,67 @@
+// { eslint settings
+/* global fm */
+// }
 fm.fns.popupJobNew=function($view) {
-	var $dialog=$('<div id="fm-popup-job-new"><table style="width:100%">'
+	var $wrapper=$('#fm-content');
+	var $shader=$('<div class="fm-dialog-shader"/>')
+		.appendTo($wrapper);
+	var $dialog=$('<div id="fm-popup-job-new" class="fm-dialog">'
+		+'<h2>Create Job</h2>'
+		+'<table style="width:100%">'
 		+'<tr><th>When</th><td><input id="fm-popup-meeting_time"/></td></tr>'
 		+'<tr><th>Job Type</th><td><select id="fm-popup-form_id"/></td></tr>'
 		+'<tr><th>Notes</th><td><textarea id="fm-popup-notes"/></td></tr>'
 		+'<tr class="fm-small"><th>Name</th><td><input id="fm-popup-name"/></td></tr>'
 		+'<tr class="fm-small"><th>Phone</th><td><input id="fm-popup-phone"/></td></tr>'
 		+'<tr class="fm-small"><th>Email</th><td><input id="fm-popup-email"/></td></tr>'
-		+'</table></div>')
-		.dialog({
-			'title':'Create Job',
-			'close':function() {
-				$dialog.remove();
-			},
-			'modal':true,
-			'buttons':{
-				'Create':function() {
-					var when=$('#fm-popup-meeting_time').val(), form_id=$('#fm-popup-form_id').val(), notes=$('#fm-popup-notes').val();
-					var name=$('#fm-popup-name').val(), phone=$('#fm-popup-phone').val(), email=$('#fm-popup-email').val();
-					if (name || phone || email) {
-						notes+="\n\n--";
-						if (name) {
-							notes+="\nName: "+name;
-						}
-						if (phone) {
-							notes+="\nPhone: "+phone;
-						}
-						if (email) {
-							notes+="\nEmail: "+email;
-						}
-					}
-					if (!/^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$/.test(when)) {
-						return alert('please choose a prefered job date/time');
-					}
-					if (!+form_id) {
-						return alert('please choose a job type');
-					}
-					$.post(fm.url+'Job_new', fm.fns.getPayLoad({'when':when, 'form_id':form_id, 'notes':notes}), function(ret) {
-						if (!ret.ok) {
-							return alert((ret||{'error':'unknown error'}).error);
-						}
-						alert('Job created');
-						$dialog.remove();
-						if ($view.draw) {
-							$view.draw();
-						}
-					});
+		+'</table>'
+		+'<div class="fm-action-buttons">'
+		+'<button class="fm-action-create">Create</button>'
+		+'<button class="fm-action-cancel">Cancel</button>'
+		+'</div></div>')
+		.appendTo($wrapper);
+	$dialog
+		.on('click', '.fm-action-cancel', ()=>{
+			$dialog.remove();
+			$shader.remove();
+		})
+		.on('click', '.fm-action-create', ()=>{
+			var when=$('#fm-popup-meeting_time').val(), form_id=$('#fm-popup-form_id').val(), notes=$('#fm-popup-notes').val();
+			var name=$('#fm-popup-name').val(), phone=$('#fm-popup-phone').val(), email=$('#fm-popup-email').val();
+			if (name || phone || email) {
+				notes+='\n\n--';
+				if (name) {
+					notes+='\nName: '+name;
+				}
+				if (phone) {
+					notes+='\nPhone: '+phone;
+				}
+				if (email) {
+					notes+='\nEmail: '+email;
 				}
 			}
-		}).parent().css('z-index', 1);
+			if (!/^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]$/.test(when)) {
+				return alert('please choose a prefered job date/time');
+			}
+			if (!+form_id) {
+				return alert('please choose a job type');
+			}
+			$.post(fm.url+'Job_new',
+				fm.fns.getPayLoad({'when':when, 'form_id':form_id, 'notes':notes}),
+				function(ret) {
+					if (!ret.ok) {
+						return alert((ret||{
+							'error':'unknown error'
+						}).error);
+					}
+					alert('Job created');
+					$dialog.find('.fm-action-cancel').click();
+					if ($view.draw) {
+						$view.draw();
+					}
+				}
+			);
+		});
 	$('#fm-popup-meeting_time').val((new Date).toYMDHIS());
 	$('#fm-popup-name').val(fm.contact.name);
 	$('#fm-popup-email').val(fm.contact.email);
@@ -58,7 +72,7 @@ fm.fns.popupJobNew=function($view) {
 				'format': 'Y-m-d H:i:s',
 				'minDate':0,
 				'formatTime':(fm.time_format=='24h'?'H:i':'g:i A'),
-			})
+			});
 	});
 	function showForms() {
 		if (!fm.forms_pickable) {
@@ -81,4 +95,4 @@ fm.fns.popupJobNew=function($view) {
 		$('#fm-popup-form_id').append(opts);
 	}
 	showForms();
-}
+};
