b80249220fbf8c6cce35a44e9387108ac7380d87
Kae Verens <kae.verens@fieldmotion.com>
2019-03-12 11:42:05 +0000

    fix issue where logout on client-agnostic site does not clear the client_id

--- logout.js
+++ logout.js
@@ -1,12 +1,15 @@
+// {  eslint settings
+/* global fm */
+// }
 fm.fns.logout=function() {
-	$.post(fm.url+'Login/logout.php', fm.fns.getPayLoad({'ok':1}), function(ret) {
+	$.post(fm.url+'Login/logout.php', fm.fns.getPayLoad({'ok':1}), function() {
 		delete localStorage.fm_cp_credentials;
-		$.each(fm, function(k, v) {
-			if (k=='$wrapper' || k=='fns' || k=='client_id' || k=='url' || k=='scriptUrl') { // don't remove these
+		$.each(fm, function(k) {
+			if (k=='$wrapper' || k=='fns' || (k=='client_id' && fm.$wrapper.data('cid')==fm.client_id) || k=='url' || k=='scriptUrl') { // don't remove these
 				return;
 			}
 			delete fm[k];
 		});
 		fm.fns.pageLogin();
 	});
-}
+};
