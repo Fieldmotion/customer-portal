6618507917ef499ffa1a25b04680c145abebc319
Kae Verens <kae.verens@fieldmotion.com>
2019-11-19 14:54:37 +0000

    see crm#1572 - portal now respects daily slots limit

--- popupJobNew.js
+++ popupJobNew.js
@@ -8,15 +8,15 @@ fm.fns.popupJobNew=function($view) {
 	var $dialog=$('<div id="fm-popup-job-new" class="fm-dialog">'
 		+'<h2>Create Job</h2>'
 		+'<table style="width:100%">'
-		+'<tr><th>When</th><td><input id="fm-popup-meeting_time"/></td></tr>'
 		+'<tr><th>Job Type</th><td><select id="fm-popup-form_id"/></td></tr>'
+		+'<tr><th>Preferred Time</th><td><input id="fm-popup-meeting_time" disabled autocomplete="off"/></td></tr>'
 		+'<tr><th>Notes</th><td><textarea id="fm-popup-notes"/></td></tr>'
 		+'<tr class="fm-small"><th>Name</th><td><input id="fm-popup-name"/></td></tr>'
 		+'<tr class="fm-small"><th>Phone</th><td><input id="fm-popup-phone"/></td></tr>'
 		+'<tr class="fm-small"><th>Email</th><td><input id="fm-popup-email"/></td></tr>'
 		+'</table>'
 		+'<div class="fm-action-buttons">'
-		+'<button class="fm-action-create">Create</button>'
+		+'<button class="fm-action-create" disabled>Create</button>'
 		+'<button class="fm-action-cancel">Cancel</button>'
 		+'</div></div>')
 		.appendTo($wrapper);
@@ -62,16 +62,58 @@ fm.fns.popupJobNew=function($view) {
 				}
 			);
 		});
-	$('#fm-popup-meeting_time').val((new Date).toYMDHIS());
 	$('#fm-popup-name').val(fm.contact.name);
 	$('#fm-popup-email').val(fm.contact.email);
 	$('#fm-popup-phone').val(fm.contact.mobile);
 	fm.fns.requireDateTimePicker(function() {
 		$('#fm-popup-meeting_time')
 			.datetimepicker({
-				'format': 'Y-m-d H:i:s',
-				'minDate':0,
-				'formatTime':(fm.time_format=='24h'?'H:i':'g:i A'),
+				format: 'Y-m-d H:i:s',
+				minDate:0,
+				value:'',
+				allowBlank:true,
+				defaultSelect:false,
+				formatTime:(fm.time_format=='24h'?'H:i':'g:i A'),
+				onGenerate:()=>{
+					var $cal=$('.xdsoft_datepicker.active');
+					if (!$cal.length) {
+						return;
+					}
+					var $td=$cal.find('tbody tr:first-child td:first-child');
+					var d=[$td.data('year'), $td.data('month'), $td.data('date')];
+					var form_id=$('#fm-popup-form_id').val();
+					var $current=$cal.find('.xdsoft_date.xdsoft_current');
+					var $tds=$cal.find('.xdsoft_date:not(.xdsoft_disabled)');
+					$tds.addClass('xdsoft_disabled');
+					$.post(fm.url+'Form_checkSlots', fm.fns.getPayLoad({id:form_id,start:d}), ret=>{
+						if (!$current.length) {
+							$cal.next('.xdsoft_timepicker').removeClass('active');
+						}
+						if (!ret || !ret.slots || !ret.slots.enabled) {
+							$tds.removeClass('xdsoft_disabled');
+						}
+						else {
+							var ds={};
+							if (ret.slots.upcoming) {
+								ret.slots.upcoming.forEach(u=>{
+									ds[u.d]=+u.ids;
+								});
+							}
+							$tds.each((k, v)=>{
+								var $td=$(v), date=$td.data('year')+'-'+(+$td.data('month')+1)+'-'+$td.data('date');
+								date=date.replace('/-([1-z])-/', '-0$1-');
+								date=date.replace('/-([1-z])$/', '-0$1');
+								if (!ds[date] || ds[date]<ret.slots.slots) {
+									$td.removeClass('xdsoft_disabled');
+								}
+							});
+						}
+					});
+				},
+				onSelectDate:()=>{
+					var $cal=$('.xdsoft_datepicker.active');
+					$cal.next('.xdsoft_timepicker').addClass('active');
+				}
 			});
 	});
 	function showForms() {
@@ -92,7 +134,17 @@ fm.fns.popupJobNew=function($view) {
 		for (var i=0;i<fm.forms_pickable.length;++i) {
 			opts.push($('<option value="'+fm.forms_pickable[i].id+'"/>').text(fm.forms_pickable[i].name));
 		}
-		$('#fm-popup-form_id').append(opts);
+		$('#fm-popup-form_id')
+			.append(opts)
+			.change(function() {
+				var id=$(this).val();
+				if (id) {
+					$('#fm-popup-meeting_time').prop('disabled', false);
+				}
+				else {
+					$('#fm-popup-meeting_time').val('').prop('disabled', true);
+				}
+			});
 	}
 	showForms();
 };
