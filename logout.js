5f3254cf075669d148649bed6556790c40568c0a
Kae Verens <kae.verens@fieldmotion.com>
2020-07-06 09:02:27 +0000

    fixes customer-portal#3

--- logout.js
+++ logout.js
@@ -4,8 +4,9 @@
 fm.fns.logout=function() {
 	$.post(fm.url+'Login/logout.php', fm.fns.getPayLoad({'ok':1}), function() {
 		delete localStorage.fm_cp_credentials;
+		var $script=$('#fm-customer-portal'), script_cid=+$script.data('cid')||0;
 		$.each(fm, function(k) {
-			if (k=='$wrapper' || k=='fns' || (k=='client_id' && fm.$wrapper.data('cid')==fm.client_id) || k=='url' || k=='scriptUrl') { // don't remove these
+			if (k=='$wrapper' || k=='fns' || (k=='client_id' && (fm.$wrapper.data('cid')==fm.client_id || fm.client_id==script_cid)) || k=='url' || k=='scriptUrl') { // don't remove these
 				return;
 			}
 			delete fm[k];
