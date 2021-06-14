7578de5921a7cf434d4d71ca97a597c7947b6ea5
Kae Verens <kae.verens@fieldmotion.com>
2021-06-14 13:05:05 +0000

    fixes #25

--- showJobsCalendar.js
+++ showJobsCalendar.js
@@ -14,7 +14,7 @@ fm.fns.showJobsCalendar=function() {
 		var $wrapper=$('#fm-calendar');
 		$wrapper.fullCalendar('destroy');
 		$wrapper.fullCalendar({ // adding Calendar
-			'eventSources': [
+			eventSources: [
 				{	//first source = appointments for customers & child companies
 					'events': function(start, end, timezone, callback){
 						$.post(fm.url+'Jobs_getCalendarCustomers', fm.fns.getPayLoad({'start':start.unix(), 'end':end.unix()}), function(ret) {
@@ -34,11 +34,93 @@ fm.fns.showJobsCalendar=function() {
 					'backgroundColor': '#385A6B'
 				}
 			],
-			'eventRender': function(event, element){
-				element.find('.fc-event-inner').prepend($('<div class="fm-job_ref" title="Job Ref"/>').text(event.job_ref));
+			eventRender: (event, element)=>{
+				$(element).closest('.fc-event').data('details', event);
+				element.find('.fc-title').prepend($('<span class="fm-job_ref"/>').text(event.job_ref+': '));
 			},
-			'timeFormat':'H:mm',
-			'disableResizing':true
+			timeFormat:'H:mm',
+			disableResizing:true
+		});
+		$wrapper.tooltip({
+			items:'.fc-event',
+			content:function() {
+				var details=$(this).data('details');
+				var $el=$('<div/>').text('...');
+				getCustomerDetails(details.customer_type, details.customer_id, obj=>{
+					var $table=$('<table><tr><th>For</th><td class="fm-job-for"/></tr></table>');
+					var forName=obj.name;
+					if (obj.parent) {
+						forName=obj.parent.name+' » '+forName;
+					}
+					$table.find('.fm-job-for').text(forName);
+					$table.appendTo($el.empty());
+					setTimeout(()=>{
+						$el.closest('.ui-tooltip').css('opacity', 1);
+					}, 100);
+				});
+				return $el;
+			}
 		});
 	});
+	var customerDetails={};
+	function getCustomerDetails(type, id, callback) {
+		if (customerDetails[type+'_'+id]) {
+			return callback(customerDetails[type+'_'+id]);
+		}
+		else {
+			if (type===1) { // asset
+				$.post(fm.url+'Asset_get', fm.fns.getPayLoad({
+					id:id
+				}), ret=>{
+					if (ret.error) {
+						return callback(ret);
+					}
+					var obj=ret.obj;
+					customerDetails[type+'_'+id]=obj;
+					var p_id=0;
+					if (+obj.location_id && +obj.location_type==1) {
+						p_id=+ret.obj.location_id;
+					}
+					else if (+obj.owner_id>0) {
+						p_id=+obj.owner_id;
+					}
+					if (p_id) {
+						$.post(fm.url+'Customer_get', fm.fns.getPayLoad({
+							id:p_id
+						}), ret=>{
+							obj.parent=ret.obj;
+							customerDetails[type+'_'+id]=obj;
+							callback(obj);
+						});
+					}
+					else {
+						callback(obj);
+					}
+				});
+			}
+			else {
+				$.post(fm.url+'Customer_get', fm.fns.getPayLoad({
+					id:id
+				}), ret=>{
+					if (ret.error) {
+						return callback(ret);
+					}
+					var obj=ret.obj;
+					customerDetails[type+'_'+id]=obj;
+					if (+obj.parent>0) {
+						$.post(fm.url+'Asset_get', fm.fns.getPayLoad({
+							id:obj.parent
+						}), ret=>{
+							obj.parent=ret.obj;
+							customerDetails[type+'_'+id]=obj;
+							callback(obj);
+						});
+					}
+					else {
+						callback(obj);
+					}
+				});
+			}
+		}
+	}
 };
